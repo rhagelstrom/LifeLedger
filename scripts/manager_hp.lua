7b82d5547f5be64c8227ac48442008f9f6fa96ea
Ryan Hagelstrom <rhagelstrom@gmail.com>
2024-07-27 11:38:20 -0500

    MAXHP with BCEG will accept a decimal for a percentage adjustment

--- scripts/manager_hp.lua
+++ scripts/manager_hp.lua
@@ -420,7 +420,13 @@ function getConAdjustment(nodeChar)
 end
 
 function getEffectAdjustment(nodeChar)
-	local nMod = EffectManager5E.getEffectsBonus(nodeChar, "MAXHP", true);
+	local nMod, _, nTotalPercent = EffectManager5E.getEffectsBonus(nodeChar, "MAXHP", true);
+	if nTotalPercent and nTotalPercent ~= 0 then
+		local fields = HpManager.getHealthFields(nodeChar);
+		local nBase = DB.getValue(nodeChar, fields.base, 0);
+		local nAdjust = DB.getValue(nodeChar, fields.adjust, 0);
+		nMod = math.floor((nBase + nAdjust + nMod) * nTotalPercent + nMod);
+	end
 	return nMod;
 end
 
